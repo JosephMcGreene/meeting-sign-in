47be3e37a991e47cf61318f2b00df7e30dd1d843
Joseph McGreene <josephgreene693@gmail.com>
2022-05-07 11:36:35 -0700

    Fix []responses interface shape

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@
 // !    - Easy and Fast to use
 // !    - Secure Server Sign-In
 // !    - Answers to sign-in questions to be used during the course of the meeting via a projector
-// TODO (1) Finish learning the necessary Formik to make the form the best it can be!
-// TODO    - https://formik.org/docs/tutorial
 // TODO (2) Add 4th and 5th questions to form (see past iterations of this project)
 // TODO    - Add modals so Kevin can edit the form each month
 // TODO (3) Add a Back-End
@@ -26,7 +24,10 @@ export default function App() {
 
 	return (
 		<Routes>
-			<Route path="/" element={<MeetingForm onSubmit={addResponse} />} />
+			<Route
+				path="/meeting-sign-in"
+				element={<MeetingForm onSubmit={addResponse} />}
+			/>
 			<Route
 				path="/responses"
 				element={<ResponseTable responses={responses} />}
--- src/components/responses-table/Body.js
+++ src/components/responses-table/Body.js
@@ -6,8 +6,8 @@ export default function Body({ responses }) {
 					<Row
 						key={responses.indexOf(cell)}
 						name={cell.name}
-						personal={cell.personal}
 						business={cell.business}
+						personal={cell.personal}
 						relationships={cell.relationships}
 					/>
 				);
@@ -21,8 +21,8 @@ function Row({ name, personal, business, relationships }) {
 	return (
 		<tr className="tbody-row">
 			<td className="tbody-cell">{name}</td>
-			<td className="tbody-cell">{personal}</td>
 			<td className="tbody-cell">{business}</td>
+			<td className="tbody-cell">{personal}</td>
 			<td className="tbody-cell">{relationships}</td>
 			<td className="tbody-cell">[Something]</td>
 			<td className="tbody-cell">[Something]</td>
