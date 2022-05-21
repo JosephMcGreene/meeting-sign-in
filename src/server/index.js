2ba5cc2e672dff4addb6532bfde3ea84c96861e2
Joseph McGreene <josephgreene693@gmail.com>
2022-05-20 19:29:33 -0700

    Add cookie info, serialize/deserialize users

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const keys = require("./config/keys");
-require("./routes/authRoutes")(app);
-require("./services/passport");
 require("./models/User");
+require("./services/passport");
 
 mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey],
+	})
+);
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- src/server/models/User.js
+++ src/server/models/User.js
@@ -3,7 +3,9 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
 	googleID: String,
-	name: String,
+	firstName: String,
+	lastName: String,
 });
 
+// 2 args to .model() means I want to load something IN to mongoose
 mongoose.model("users", userSchema);
